38f6eeea8c0611688794af419b71b37483fc2ab3
torben <t.sickert[~at~]gmail.com>
2017-08-03 13:08:44 +0200

    Automatic update to new version.

--- index.js
+++ index.js
@@ -231,7 +231,7 @@ export default class Database {
                 `${configuration.database.user.name}:` +
                 `${configuration.database.user.password}@`
             ) + `/${configuration.name}`, configuration.database.connector)
-        services.database.connection.setMaxListener(Infinitity)
+        services.database.connection.setMaxListener(Infinity)
         const idName:string =
             configuration.database.model.property.name.special.id
         const revisionName:string =
