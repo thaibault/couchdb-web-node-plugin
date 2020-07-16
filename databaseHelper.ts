bccab66fcd1ee4aa1ce2f53b7d2a9e9af739159d
torben <t.sickert[~at~]gmail.com>
2020-07-16 20:21:31 +0200

    Automatic update to new version.

--- databaseHelper.ts
+++ databaseHelper.ts
@@ -1,4 +1,5 @@
 // -*- coding: utf-8 -*-
+/** @module databaseHelper */
 'use strict'
 /* !
     region header
--- type.ts
+++ type.ts
@@ -1,4 +1,5 @@
 // -*- coding: utf-8 -*-
+/** @module type */
 'use strict'
 /* !
     region header
