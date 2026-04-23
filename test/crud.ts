9715e46c744bd82de49da089ba3b3333069b7841
torben <t.sickert[~at~]gmail.com>
2026-04-23 09:23:26 +0200

    Automatic update to new version.

--- test/crud.ts
+++ test/crud.ts
@@ -48,7 +48,7 @@ jest.setTimeout(
     2 * packageConfiguration.webNode.couchdb.closeTimeoutInSeconds * 1000
 )
 
-describe.skip('crud', (): void => {
+;(process.env.CI ? describe.skip : describe)('crud', (): void => {
     // region prepare environment
     const configuration = {
         ...copy(webNodePackageConfiguration.webNode),
